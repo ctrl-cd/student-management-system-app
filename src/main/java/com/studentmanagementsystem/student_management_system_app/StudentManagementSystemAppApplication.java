8b61ff8e1577c5ee01129fc80d815483924e54a8
ctrl-cd <rajdipsojitra5@gmail.com>
2026-06-06 12:03:50 +0530

    first commit

---
+++ src/main/java/com/studentmanagementsystem/student_management_system_app/StudentManagementSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanagementsystem.student_management_system_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class StudentManagementSystemAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementSystemAppApplication.class, args);
+  }
+}
---
+++ src/test/java/com/studentmanagementsystem/student_management_system_app/StudentManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentmanagementsystem.student_management_system_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
